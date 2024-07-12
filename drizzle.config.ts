3265af62bcdc227f0d06cf64b8e93469fe80470c
Kazyel <m.mascarelo@gmail.com>
2024-07-12 00:42:20 -0300

    MHiding database credentials

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
     dbCredentials: {
-        host: "localhost",
-        port: 5432,
-        user: "postgres",
-        password: "123",
-        database: "api_vue",
+        host: process.env.DB_HOST as string,
+        port: process.env.DB_PORT as unknown as number,
+        user: process.env.DB_USER as string,
+        password: process.env.DB_PASSWORD as string,
+        database: process.env.DB_NAME as string,
     },
     dialect: "postgresql",
     schema: "./src/db/schema.ts",
